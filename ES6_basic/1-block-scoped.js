e8a46a32f38d550a5f04d0e27524640b4be6ae2d
Mehralizada <gulanemehrelizade97@gmail.com>
2024-07-20 18:52:42 +0000

    file updated

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,14 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    // Use separate variables for the block scope
+    let blockTask = true;
+    let blockTask2 = false;
+
+    // Optionally, you can assign these to the outer variables
+    // if you want to use them outside the block
+    task = blockTask;
+    task2 = blockTask2;
   }
 
   return [task, task2];
