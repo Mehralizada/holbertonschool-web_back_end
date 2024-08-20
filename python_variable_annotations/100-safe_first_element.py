2c20b67c076df926fa7958719b620cc82300e1de
Mehralizada <gulanemehrelizade97@gmail.com>
2024-08-20 07:25:01 +0000

    file created

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ This module provides a function to safely get the first element of a sequence.
 
 from typing import Sequence, Any, Optional
 
+
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
     Return the first element of a sequence if it exists, else None.
