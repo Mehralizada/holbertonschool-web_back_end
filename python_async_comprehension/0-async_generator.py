c44d67fd1c8cb500ecdb83df197cd07fe79eeadf
Mehralizada <gulanemehrelizade97@gmail.com>
2024-08-22 15:30:16 +0000

    file updated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,8 +10,8 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """Function for asynchronous generator."""
     for _ in range(10):
-        await asyncio.sleep(1)
+        await asyncio.sleep(1)  # It will wait a second
         yield random.uniform(0, 10)
